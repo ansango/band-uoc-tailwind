7c4b8418549f7f81c30f10b4c5f11574cb72ddbf
Anibal Santos <anibalsantosgo@gmail.com>
2021-05-03 23:26:41 +0200

    clean

--- src/assets/scripts/app/components/Nav/index.js
+++ src/assets/scripts/app/components/Nav/index.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { NavLink } from "react-router-dom";
 import { IconOpen, IconClose, MobileMenu, Links, HomeLink } from "./BlocksNav";
 
 const Nav = () => {

8551c91e3d86021962cbad8e49fd3d6f9444321e
Anibal Santos <anibalsantosgo@gmail.com>
2021-05-24 22:38:02 +0200

    table

--- src/assets/scripts/app/components/Table/index.js
+++ src/assets/scripts/app/components/Table/index.js
@@ -2,32 +2,29 @@ import React from "react";
 import PropTypes from "prop-types";
 import { FaTicketAlt } from "react-icons/fa";
 const Table = (props) => {
+  const appoints = props.appoints.map((appoint, index) => {
+    let date = `${appoint.date.month} ${appoint.date.day} ${appoint.date.dayWeek}`;
+    let country = `${appoint.location[1].region}`;
+    let time = `${appoint.hour}`;
+    let place = `${appoint.location[0].place}`;
+    let event = `${place} | ${time}`;
+    return (
+      <tr key={index}>
+        <td className="icon">
+          <FaTicketAlt size={20} />
+        </td>
+        <td className="data">{date}</td>
+        <td className="data">{event}</td>
+        <td className="data">{country}</td>
+        <td className="action">
+          <button>Tickets</button>
+        </td>
+      </tr>
+    );
+  });
   return (
-    <table className="table-auto">
-      <tbody className="divide-y divide-gray">
-        {props.appoints.map((appoint, index) => {
-          let date = `${appoint.date.month} ${appoint.date.day} ${appoint.date.dayWeek}`;
-          let country = `${appoint.location[1].region}`;
-          let time = `${appoint.hour}`;
-          let place = `${appoint.location[0].place}`;
-          let event = `${place} | ${time}`;
-          return (
-            <tr key={index}>
-              <td className="px-6 py-4 text-primary">
-                <FaTicketAlt size={20} />
-              </td>
-              <td className="px-6 py-4 xl:px-10">{date}</td>
-              <td className="px-6 py-4 xl:px-20">{event}</td>
-              <td className="px-6 py-4 xl:px-20">{country}</td>
-              <td className="px-6 py-4">
-                <button className="bg-primary rounded-md px-3 py-2 text-white">
-                  Tickets
-                </button>
-              </td>
-            </tr>
-          );
-        })}
-      </tbody>
+    <table className="table">
+      <tbody>{appoints}</tbody>
     </table>
   );
 };

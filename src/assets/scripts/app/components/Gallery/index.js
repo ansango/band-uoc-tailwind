3815fea45e7d9bbdc4f912a56f61dd4a6b788d19
Anibal Santos <anibalsantosgo@gmail.com>
2021-05-24 22:22:08 +0200

    gallery

--- src/assets/scripts/app/components/Gallery/index.js
+++ src/assets/scripts/app/components/Gallery/index.js
@@ -1,19 +1,18 @@
 import React from "react";
 import images from "../../data/gallery";
+
+const figures = images.map((image, index) => {
+  return (
+    <figure key={index}>
+      <img src={image.url} alt={image.name} />
+    </figure>
+  );
+});
+
 const Gallery = () => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-      {images.map((image, index) => {
-        return (
-          <figure key={index}>
-            <img
-              src={image.url}
-              className="transition hover:opacity-70 ease-in-out"
-              alt={image.name}
-            />
-          </figure>
-        );
-      })}
+    <div className="gallery">
+      <div>{figures}</div>
     </div>
   );
 };
--- src/assets/scripts/app/views/Tour.js
+++ src/assets/scripts/app/views/Tour.js
@@ -1,26 +1,17 @@
 import React from "react";
 import Parallax from "../components/Parallax";
-import Ticket from "../components/Card/ticket";
-import appoints from "../data/appoints";
+import Appoints from "../components/Appoints";
 import tour from "../../../images/parallax/tour.jpg";
 import Table from "../components/Table";
 import Gallery from "../components/Gallery";
+
 const Tour = () => {
   return (
     <>
       <Parallax img={tour} title={"TOUR 2021"} />
       <div className="view">
-        <div className="md:hidden max-w-lg mx-auto px-2">
-          {appoints.map((appoint, index) => {
-            return <Ticket key={index} appoint={appoint} />;
-          })}
-        </div>
-        <div className="hidden md:flex justify-center">
-          <Table appoints={appoints} />
-        </div>
-        <div className="max-w-lg md:max-w-none mx-auto px-2 py-10">
-          <Gallery />
-        </div>
+        <Appoints />
+        <Gallery />
       </div>
     </>
   );
